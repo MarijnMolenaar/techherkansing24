4dddacdaaee9613555e8f11ee12e827effbed30b
MarijnMolenaar <32360693+MarijnMolenaar@users.noreply.github.com>
2024-04-03 13:23:12 +0200

    Bestanden upload van 3 april 2024

--- README.md
+++ README.md
@@ -55,6 +55,38 @@ Pug-bestanden zijn te vinden in de `views` map. Voor het toevoegen of bewerken v
 
 Gebruik MongoDB Compass voor het visualiseren en beheren van de MongoDB-database. Je kunt databases creëren, documenten bekijken/bewerken/verwijderen en queries uitvoeren.
 
+## Maps API
+
+Integratie van Google Maps Embed in je Project
+De Google Maps Embed API stelt ontwikkelaars in staat om Google Maps naadloos in hun applicaties en websites te integreren, waardoor gebruikers direct toegang hebben tot kaarten en locatiegegevens. Hieronder vindt u een beknopte handleiding over hoe u deze functionaliteit in je project kunt opnemen.
+
+Stap 1: Verkrijgen van een API-sleutel
+Om gebruik te maken van de Google Maps Embed API, is het essentieel om een API-sleutel te verkrijgen. Deze sleutel identificeert je applicatie bij de Google API-services en zorgt voor toegang. Volg deze stappen om een sleutel te genereren:
+
+Bezoek de Google Cloud Console en log in met je Google-account.
+Maak een nieuw project of selecteer een bestaand project.
+Navigeer naar de 'API's & Services' > 'Credentials' pagina.
+Klik op 'Create Credentials' en kies 'API key'. De aangemaakte sleutel wordt getoond; noteer deze veilig.
+Stap 2: De Embed Code Integreren
+Nadat u je API-sleutel heeft verkregen, kunt u de Google Maps Embed API in je project integreren. Voeg de volgende HTML-code toe aan je website of applicatie op de plaats waar u de kaart wilt weergeven:
+
+<iframe
+  width="600"
+  height="450"
+  style="border:0"
+  loading="lazy"
+  allowfullscreen
+  src="https://www.google.com/maps/embed/v1/place?key=UW_API_SLEUTEL&q=PlaceName">
+</iframe>
+Vervang UW_API_SLEUTEL met je daadwerkelijke API-sleutel en PlaceName met de gewenste locatie.
+
+Stap 3: Aanpassingen en Gebruik
+De Embed API biedt verschillende mogelijkheden om de weergave en functies van de kaart aan te passen. U kunt de documentatie van de Google Maps Embed API raadplegen voor meer informatie over aanpassingsopties en geavanceerde functionaliteiten.
+
+## Unsplash CAR API
+
+Voor het integreren van het ophalen van willekeurige foto's via de Unsplash API in je project, moet je een API-sleutel hebben en het /photos/random endpoint gebruiken. Dit endpoint staat je toe om een of meerdere willekeurige foto's te ontvangen, met opties voor specificatie zoals thema's of zoekwoorden. Je kunt dit doen door een HTTP GET-verzoek te versturen, inclusief je API-sleutel in de header voor authenticatie. Voor details over het constructen van je verzoek en het verwerken van de respons, bekijk de Unsplash API documentatie; https://unsplash.com/documentation#get-a-random-photo
+
 ## Bijdragen
 
 Wil je bijdragen? Volg deze stappen:
--- index.js
+++ index.js
@@ -27,22 +27,7 @@ async function connect() {
     }
 }
 
-// Client-side API code
-const autoData = [
-  
-    { _id: '65df0ed431dd7777cd9f759c', name: 'Audi S5', bouwjaar: '2015', kilometerstand: '34000', vraagprijs: '54000', bod: '$50000' },
-    { _id: '21ae4edd3b8f8fe9daa1b19a', name: 'BMW M3', bouwjaar: '2018', kilometerstand: '22000', vraagprijs: '65000', bod: '$62000' },
-    { _id: '58cb137c8d8e204ee8d2e334', name: 'Mercedes AMG GT', bouwjaar: '2019', kilometerstand: '12000', vraagprijs: '98000', bod: '$95000' },
-    { _id: '9d3f5c8501fbd892358b273e', name: 'Tesla Model S', bouwjaar: '2020', kilometerstand: '5000', vraagprijs: '75000', bod: '$72000' },
-    { _id: '3f45ba9f5e918f761d62e2ea', name: 'Volvo XC90', bouwjaar: '2017', kilometerstand: '45000', vraagprijs: '40000', bod: '$38000' },
-    { _id: 'aefb8c47d3a80f18134131a3', name: 'Ford Mustang GT', bouwjaar: '2016', kilometerstand: '30000', vraagprijs: '32000', bod: '$31000' },
-    { _id: '4ceda0b8b50eee4038e77e83', name: 'Porsche 911', bouwjaar: '2018', kilometerstand: '15000', vraagprijs: '112000', bod: '$110000' },
-    { _id: '2f3ae3b8c13237b3d2e9c3b4', name: 'Honda Civic Type R', bouwjaar: '2019', kilometerstand: '10000', vraagprijs: '35000', bod: '$34000' },
-    { _id: '6e5f8e9d2b9c5817f5d3e6ab', name: 'Nissan GT-R', bouwjaar: '2017', kilometerstand: '20000', vraagprijs: '90000', bod: '$88000' },
-    { _id: '8d9f3b1c2a3b59127d3e7e92', name: 'Lamborghini Huracan', bouwjaar: '2020', kilometerstand: '8000', vraagprijs: '200000', bod: '$195000' }
-  
 
-];
 
 app.get('/api/autos', async (req, res) => {
   const collection = await connect();
@@ -55,12 +40,18 @@ app.get('/api/autos', async (req, res) => {
 app.get('/', async (req, res) => {
     const collection = await connect();
     const autos = await collection.find().toArray(); // Alle auto's ophalen
-    res.render('index', { autos, autoData }); // Render de indexpagina met de auto's
+    res.render('index', { autos }); // Render de indexpagina met de auto's
 });
 
 app.get('/showroom', async (req, res) => {
     res.render('showroom'); // Render de showroom pagina
 });
+
+app.get('/overons', async (req, res) => {
+    res.render('overons'); // Render de showroom pagina
+});
+
+
 
 // Route: Update de vraagprijs van een auto (bron: ChatGPT4)
 app.post('/update-auto', async (req, res) => {
@@ -79,7 +70,7 @@ app.post('/update-auto', async (req, res) => {
         // Veronderstelt dat 'bod' in de database al als een integer is opgeslagen
         if (nieuwBod <= auto.bod) {
             // Vervang 'alert' met een methode die werkt op de server, zoals het sturen van een foutmelding terug naar de client
-            return res.status(400).send('Nieuw bod moet hoger zijn dan het huidige bod.');
+            return res.status(400).send('Je bod moet hoger zijn dan het hoogste bod!');
         }
 
         await autosCollection.updateOne(
@@ -98,5 +89,5 @@ app.post('/update-auto', async (req, res) => {
 
 // Server starten
 app.listen(3000, () => {
-    console.log("Server draait op http://localhost:3000/ ");
+    console.log("Server draait op http://localhost:3000/");
 });
